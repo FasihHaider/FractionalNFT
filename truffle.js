b6bf19381d9e6caf21032558d055ef64050bacd0
Fasih <fasih.haider@kryptomind.com>
2022-03-08 17:31:26 +0500

    Update truffle config

--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const mnemonic = "egg dust custom conduct example miracle disease person aunt tone group royal";
+const mnemonic = "";
 
 module.exports = {
   networks: {
@@ -21,31 +21,6 @@ module.exports = {
       confirmations: 10,
       timeoutBlocks: 200,
       skipDryRun: true
-    },
-    kovan: {
-      provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/3dfb5f4a4b564ee893955cd379a3e449`),
-      network_id: 42,
-      gas: 4000000
-    },
-    rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/3dfb5f4a4b564ee893955cd379a3e449`),
-      network_id: 4,
-      gas: 4000000
-    },
-    ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/3dfb5f4a4b564ee893955cd379a3e449`),
-      network_id: 3,
-      gas: 4000000
-    },
-    ethereum: {
-      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/3dfb5f4a4b564ee893955cd379a3e449`),
-      network_id: 1,
-      gas: 4000000
-    },
-    ftmtest: {
-      provider:() => new HDWalletProvider(mnemonic, "https://rpc.testnet.fantom.network/"),
-      network_id: 0xfa2,
-      timeoutBlocks : 20000
     }
   },
   compilers: {
